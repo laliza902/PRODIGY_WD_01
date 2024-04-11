8e2e57eaf9d67a5a830a8f3fe37db086e091b7d3
laliza902 <lizakalita040@gmail.com>
2024-04-11 21:49:45 +0530

    final

---
+++ script.js
@@ -0,0 +1,11 @@
+const nav_items = document.querySelectorAll('#items a');
+
+nav_items.forEach(item =>{
+    item.addEventListener('mouseover',()=>{
+        item.style.color = 'black';
+    });
+
+    item.addEventListener('mouseout',() =>{
+        item.style.color = 'whitesmoke';
+    });
+});
